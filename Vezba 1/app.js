833181893747d212babe1ce7756f343c61fc2369
HamzaPapicc <hamza70.hp@gmail.com>
2022-05-19 19:32:17 +0200

    first commit

--- Vezba 1/app.js
+++ Vezba 1/app.js
@@ -8,11 +8,24 @@
 // console.log(result)
 // console.log(result/niz.length);
 
-howMuch = parseInt(prompt("How many numbers?"));
-niz = [];
+// howMuch = parseInt(prompt("How many numbers?"));
+// niz = [];
 
-for (i=0; i<howMuch; i++) {
-    whatNum = parseInt(prompt("What number?"));
-    niz.push(whatNum);
+// for (i=0; i<howMuch; i++) {
+//     whatNum = parseInt(prompt("What number?"));
+//     niz.push(whatNum);
+// }
+// console.log(niz);
+
+niz = [3, 5, 10, 1, 7];
+drugiNiz = [];
+
+// for (i=0; i<niz.length; i++) {
+//     drugiNiz.unshift(niz[i])
+// }
+// console.log(drugiNiz);
+
+for (i=niz.length-1; i>=0; i--) {
+    drugiNiz.push(niz[i]);
 }
-console.log(niz);
+console.log(drugiNiz);
